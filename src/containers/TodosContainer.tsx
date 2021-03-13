325bc43e0537d2c43d0f13864b798cb16720e516
formegusto <formegusto@gmail.com>
2021-03-13 14:21:42 +0900

    Todos Connected Component

--- src/containers/TodosContainer.tsx
+++ src/containers/TodosContainer.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import TodosComponent from '../components/TodosComponent';
+import { RootStore } from '../store';
+import { todoActions } from '../store/todos/actions';
+import { TodoItem } from '../store/todos/types';
 
-function TodosContainer() {
+type Props = {
+    todos: TodoItem[];
+    TodoActions: typeof todoActions;
+};
+
+function TodosContainer(props: Props) {
     return <TodosComponent />;
 }
 
-export default TodosContainer;
+export default connect(
+    ({todo}: RootStore) => ({todos: todo.todos}),
+    (dispatch) => ({TodoActions : bindActionCreators(todoActions, dispatch)})
+)(TodosContainer);
